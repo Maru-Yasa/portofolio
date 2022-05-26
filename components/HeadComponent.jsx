8f550dd21891933e17d94cac4544555ccd3c1a18
maru-yasa <marufilyasa13@gmail.com>
2022-05-26 12:33:16 +0700

    make head component

---
+++ components/HeadComponent.jsx
@@ -0,0 +1,24 @@
+
+export function HeadComponent(){
+    return (
+
+        <Head>
+        <meta charSet="utf-8"/>
+        <title>Maru Yasa | Portofolio</title>
+        <meta name="description" content="HI There! 👋, I'm Maru Yasa, and this is my portofolio website" />
+        <meta name="image" content="https://media-exp1.licdn.com/dms/image/C4E03AQHoYXy-4o1BFA/profile-displayphoto-shrink_200_200/0/1645504624862?e=1658966400&v=beta&t=B1rbCKZsTkyAuxwLo1s80kPaONn5_CxWh6SVzq9ceNw" />
+        <meta itemProp="name" content="Maru Yasa" />
+        <meta itemProp="description" content="HI There! 👋, I'm Maru Yasa, and this is my portofolio website" />
+        <meta itemProp="image" content="https://media-exp1.licdn.com/dms/image/C4E03AQHoYXy-4o1BFA/profile-displayphoto-shrink_200_200/0/1645504624862?e=1658966400&v=beta&t=B1rbCKZsTkyAuxwLo1s80kPaONn5_CxWh6SVzq9ceNw" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Maru Yasa" />
+        <meta name="twitter:description" content="HI There! 👋, I'm Maru Yasa, and this is my portofolio website" />
+        <meta name="og:title" content="Maru Yasa" />
+        <meta name="og:description" content="HI There! 👋, I'm Maru Yasa, and this is my portofolio website" />
+        <meta name="og:image" content="https://media-exp1.licdn.com/dms/image/C4E03AQHoYXy-4o1BFA/profile-displayphoto-shrink_200_200/0/1645504624862?e=1658966400&v=beta&t=B1rbCKZsTkyAuxwLo1s80kPaONn5_CxWh6SVzq9ceNw" />
+        <meta name="og:site_name" content="Maru Yasa's Portofolio" />
+        <meta name="og:type" content="website" />
+      </Head>
+
+    )
+}
